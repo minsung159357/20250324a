ab9f848971dc0b97428480f3631a3544b4070a0c
minsung159357 <minsung159357@gmail.com>
2025-04-09 09:48:13 +0900

    [UPDATE] test src

--- step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -2,12 +2,18 @@ package edu.fisa.ce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+// 또는
+import static org.junit.jupiter.api.Assertions.assertEquals; // JUnit 5
 
 @SpringBootTest
 class Step07CicdApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int result = 2 + 3;
+		System.out.println("****비교 전****");
+		
+		assertEquals(5, result); // 성공: 5 == 5
+		System.out.println("****비교 후****");
 	}
-
 }
